036af20fabebadcb75bfbb9780fda1dfb1c45784
rgarro <rgarro@gmail.com>
2023-11-20 14:48:57 -0600

    smoking and cofee break ..

---
+++ Assets/Scripts/BatComputer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**
+*      _..--""````""--.._
+*    .'       |\/|       '.
+*   /    /`._ |  | _.'\    \
+*  ;    /              \    |
+*  |   /                \   |
+*  ;  / .-.          .-. \  ;
+*   \ |/   \.-.  .-./   \| /
+*    '._       \/       _.'
+*       ''--..____..--''
+*
+*
+*
+*@author Rolando<rgarro@gmail.com>
+*/
+public class BatComputer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
